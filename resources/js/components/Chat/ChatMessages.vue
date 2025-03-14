a0a05739ead8fca4662f589c9a77a879d4363382
de-lolec <matvey20158@mail.ru>
2025-03-14 15:16:13 -0300

    dev: minor change to the settings template and chat messages area

--- resources/js/components/Chat/ChatMessages.vue
+++ resources/js/components/Chat/ChatMessages.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="chat-window flex-1 overflow-hidden">
+  <div class="flex flex-1 flex-col h-[400px] overflow-hidden">
     <div
       ref="messagesContainer"
-      class="messages-container flex-1 overflow-y-auto p-4 min-h-0"
+      class="bg-gray-50 py-5  flex-1 overflow-y-auto p-4 min-h-0"
       @scroll="onScroll"
     >
       <ChatMessage
@@ -44,11 +44,5 @@ const onScroll = () => {
   display: flex;
   flex-direction: column;
   height: 400px;
-  border: 1px solid #ccc;
-}
-
-.messages-container {
-  background-color: #f9f9f9;
-  padding: 10px;
 }
 </style>
